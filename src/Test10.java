eb82628551da0247381d57b84d816814dbf69924
Test@123 <LENOVO@DESKTOP-QI4TOR5>
2022-05-22 18:56:53 +0530

    Master : modifuied Test10.java

--- src/Test10.java
+++ src/Test10.java
@@ -1,8 +1,30 @@
 
 public class Test10 {
 
+	private long age;
+	private String city;
+	
 	public static void main(String[] args) {
+		
+		
+		
 		System.out.println("Test10");
+	}
+
+	public int getAge() {
+		return (int) age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
 	}
 
 }
